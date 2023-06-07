99fb97b7df07e876a46921dcd74e0bbfd3404619
admin <klimovpcartem@gmail.com>
2023-06-08 00:27:03 +0300

    done try 2

--- hw4.py
+++ hw4.py
@@ -1,17 +1,10 @@
-first_list=[1, 2, 3, 4, 5, 6]
+first_list=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 #first_list=[1, 2, 3]
 #first_list=[1, 2, 3, 4, 5]
 #first_list=[1]
 #first_list=[]
 
 x=len(first_list)
-if x>3:
-    my_list = [first_list[:3], first_list[3:6]]
-    print(my_list)
-elif x==3:
-    my_list = [first_list[:2], first_list[2:3]]
-    print(my_list)
-else:
-    my_list = [first_list[:1], first_list[1:2]]
-    print(my_list)
-#my_list = [first_list[:3], first_list[3:6]] // так отрабатывало но фейлилось на примере [1, 2, 3]
+y=x-(x//2)
+my_list = [first_list[0:y], first_list[y:x]]
+print(my_list)
